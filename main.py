961479fdfe817c4652a82315fb30a148a83091f9
Toshiki Hayashi <1859918+windschord@users.noreply.github.com>
2023-10-14 12:25:56 +0900

    change to use latest speedtest cli

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import re
 import subprocess
 from textwrap import dedent
 from flask import Flask, make_response, request
@@ -8,82 +7,99 @@ app = Flask(__name__)
 
 
 class SpeedTestServer:
-    def __init__(self, id, name, location, country, distance):
-        self.id = id
+    def __init__(self, server_id, name, location, country):
+        self.server_id = server_id
         self.name = name
         self.location = location
         self.country = country
-        self.distance = distance
 
     def __repr__(self):
-        return f"{self.id}: {self.name} [{self.location}, {self.country}] {self.distance}km"
+        return f"{self.server_id}: {self.name} [{self.location}, {self.country}]"
 
     def metrics_link(self):
-        return f'<a href="/metrics?server_id={self.id}">Metrics ({self.name} {self.country} {self.distance}km)</a>'
+        return f'<a href="/metrics?server_id={self.server_id}">Metrics ({self.name} {self.country})</a>'
 
+class SpeedTestException(Exception):
+    pass
 
 def speed_test(server_id=None):
-    cmd = ["speedtest", "--accept-license", "--json"]
+    cmd = "speedtest --accept-license --format=json"
     if server_id is not None:
-        cmd.append("--server")
-        cmd.append(server_id)
+        cmd += f" --server-id={server_id}"
 
     print(cmd)
     res = subprocess.run(cmd, capture_output=True, shell=True, text=True)
 
     if res.returncode != 0:
-        raise Exception(f'speedtest command failed.exit no zero. {res.stderr}')
-    elif len(res.stderr) > 0:
-        raise Exception(f'speedtest command failed {res.stderr}')
+        raise SpeedTestException(f'speedtest command failed.exit no zero. {res.stderr}')
     else:
+        if len(res.stderr) > 0:
+            print(f'speedtest command stderr:\n {res.stderr}')
         return json.loads(res.stdout)
 
 
 def speed_test_server_list():
-    cmd = ["speedtest", "--list", "--json"]
-    fmt = re.compile(r'(\d+)\) (.*) \((.*), (.*)\) \[(.*) km\]')
+    cmd = "speedtest --servers --format=json"
     ret = []
 
     res = subprocess.run(cmd, capture_output=True, shell=True, text=True, encoding='utf-8')
     if res.returncode != 0:
-        raise Exception("speedtest command failed")
+        raise SpeedTestException("speedtest command failed")
 
-    for line in res.stdout.split('\n'):
-        if len(line.strip()) == 0:
-            continue
-
-        trimmed = fmt.findall(line)
-        if len(trimmed[0]) != 5:
-            print(f"invalid line. not 5 rows: {line}")
-            continue
-        ret.append(SpeedTestServer(trimmed[0][0], trimmed[0][1], trimmed[0][2], trimmed[0][3], trimmed[0][4]))
+    for server in json.loads(res.stdout)['servers']:
+        ret.append(SpeedTestServer(server['id'], server['name'], server['location'], server['country']))
     return ret
 
 
 @app.route('/')
 def index():
-    return '<a href="/metrics">Metrics (auto)</a><br>' + '<br>'.join([s.metrics_link() for s in speed_test_server_list()])
+    return '<a href="/metrics">Metrics (auto)</a><br>' + '<br>'.join(
+        [s.metrics_link() for s in speed_test_server_list()])
 
 
 @app.route('/metrics', methods=['GET'])
 def metrics():
     try:
         st = speed_test(request.args.get("server_id"))
-
+        common_labels = f'country="{st["server"]["country"]}",id="{st["server"]["id"]}",name="{st["server"]["name"]}",ip="{st["server"]["ip"]}"'
         text = dedent(f"""
-        # HELP speed_test_ping The number of ping (ms).
-        # TYPE speed_test_ping counter
-        speed_test_ping{{country="{st["server"]["country"]}",id="{st["server"]["id"]}",host="{st["server"]["host"]}"}} {st["ping"]}
-        # HELP speed_test_download The number of download (bit/s).
-        # TYPE speed_test_download counter
-        speed_test_download{{country="{st["server"]["country"]}",id="{st["server"]["id"]}",host="{st["server"]["host"]}"}} {st["download"]}
-        # HELP speed_test_upload The number of upload (bit/s).
-        # TYPE speed_test_upload counter
-        speed_test_upload{{country="{st["server"]["country"]}",id="{st["server"]["id"]}",host="{st["server"]["host"]}"}} {st["upload"]}
+        # HELP speedtest_jitter.
+        # TYPE speedtest_jitter counter
+        speedtest_jitter{{{common_labels},type="ping"}} {st['ping']['jitter']}
+        
+        # HELP speedtest_latency.
+        # TYPE speedtest_latency counter
+        speedtest_latency{{{common_labels},type="ping"}} {st['ping']['latency']}
+        speedtest_latency{{{common_labels},type="ping_low"}} {st['ping']['low']}
+        speedtest_latency{{{common_labels},type="ping_high"}} {st['ping']['high']}
+        speedtest_latency{{{common_labels},type="download_latency"}} {st['download']['latency']['jitter']}
+        speedtest_latency{{{common_labels},type="download_iqm"}} {st['download']['latency']['iqm']}
+        speedtest_latency{{{common_labels},type="download_low"}} {st['download']['latency']['low']}
+        speedtest_latency{{{common_labels},type="download_high"}} {st['download']['latency']['high']}        
+        speedtest_latency{{{common_labels},type="upload_latency"}} {st['upload']['latency']['jitter']}
+        speedtest_latency{{{common_labels},type="upload_iqm"}} {st['upload']['latency']['iqm']}
+        speedtest_latency{{{common_labels},type="upload_low"}} {st['upload']['latency']['low']}
+        speedtest_latency{{{common_labels},type="upload_high"}} {st['upload']['latency']['high']}
+        
+        # HELP speedtest_bandwidth.
+        # TYPE speedtest_bandwidth counter
+        speedtest_bandwidth{{{common_labels},type="download"}} {st['download']['bandwidth']}
+        speedtest_bandwidth{{{common_labels},type="upload"}} {st['upload']['bandwidth']}
+        
+        # HELP speedtest_bytes.
+        # TYPE speedtest_bytes counter
+        speedtest_bytes{{{common_labels},type="download"}} {st['download']['bytes']}
+        speedtest_bytes{{{common_labels},type="upload"}} {st['upload']['bytes']}
+        
+        # HELP speedtest_elapsed.
+        # TYPE speedtest_elapsed counter
+        speedtest_elapsed{{{common_labels},type="download"}} {st['download']['elapsed']}
+        speedtest_elapsed{{{common_labels},type="upload"}} {st['upload']['elapsed']}
         """)
         response = make_response(text, 200)
         response.mimetype = "text/plain"
     except Exception as e:
+        print(f"error occurred: {e}")
         response = make_response(str(e), 500)
         response.mimetype = "text/plain"
     return response
